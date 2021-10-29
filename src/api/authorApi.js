925c2cb355f08fc82ea56eacdf77feccdb4aa04b
James W Blessing <37204075+wblessing@users.noreply.github.com>
2021-10-28 19:04:39 -0500

    Update authorApi.js

--- src/api/authorApi.js
+++ src/api/authorApi.js
@@ -1,7 +1,7 @@
 import { handleResponse, handleError } from './apiUtils';
 require('dotenv').config();
 
-const baseUrl = 'http://localhost:8888/api/authors/';
+const baseUrl = 'https://jwb-mizal-app.netlify.app/api/authors/';
 // const baseUrl = process.env.FUNC_AIRTABLE_API_URL + '/api/authors/';
 
 export function getAuthors() {
