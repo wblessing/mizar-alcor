a7016a484d13f345f9179d250b11febb62788c9d
wblessing <jwblessing@att.net>
2021-10-31 13:30:49 -0500

    add context to getCourses

--- functions/courses.js
+++ functions/courses.js
@@ -3,16 +3,17 @@ const getCourses = require('./helpers/getCourses');
 const createCourse = require('./helpers/createCourse');
 const deleteCourse = require('./helpers/deleteCourse');
 const updateCourse = require('./helpers/updateCourse');
-exports.handler = async (event) => {
-    if (event.httpMethod === 'GET') {
-        return await getCourses(event);
-    } else if (event.httpMethod === 'POST') {
-        return await createCourse(event);
-    } else if (event.httpMethod === 'PUT') {
-        return await updateCourse(event);
-    } else if (event.httpMethod === 'DELETE') {
-        return await deleteCourse(event);
-    } else {
-        return formattedReturn(405, {});
-    }
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod === 'GET') {
+    return await getCourses(event, context);
+  } else if (event.httpMethod === 'POST') {
+    return await createCourse(event, context);
+  } else if (event.httpMethod === 'PUT') {
+    return await updateCourse(event, context);
+  } else if (event.httpMethod === 'DELETE') {
+    return await deleteCourse(event, context);
+  } else {
+    return formattedReturn(405, {});
+  }
 };
--- functions/helpers/getCourses.js
+++ functions/helpers/getCourses.js
@@ -1,7 +1,7 @@
 const { coursesTable } = require('./airtable');
 const formattedReturn = require('./formattedReturn');
-module.exports = async (event) => {
-  console.log('Event is' + event);
+
+module.exports = async (event, context) => {
   try {
     const courses = await coursesTable.select().firstPage();
     const formattedCourses = courses.map((course) => ({
