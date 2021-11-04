3c2bf2022104b8ae9fef39a5fe4a71626a6d530f
Bill Blessing <wblessing@macbook.attlocal.net>
2021-11-04 07:19:03 -0500

    logging for create course function

--- functions/create-course.js
+++ functions/create-course.js
@@ -1,10 +1,11 @@
-const { table } = require('./helpers/airtable');
+const { coursesTable } = require('./helpers/airtable');
 const formattedReturn = require('./helpers/formattedReturn');
 
 exports.handler = async (event) => {
   const fields = JSON.parse(event.body);
+  console.log('fields for create ' + fields);
   try {
-    const createdCourse = await table.create([{ fields }]);
+    const createdCourse = await coursesTable.create([{ fields }]);
     return formattedReturn(200, createdCourse);
   } catch (err) {
     console.error(err);
