b2b9961b39ba84172c851656a1d1d29d7158b5b2
Bill Blessing <wblessing@macbook.attlocal.net>
2021-11-04 06:26:47 -0500

    add exports.handler to create course

--- functions/create-course.js
+++ functions/create-course.js
@@ -1,6 +1,7 @@
 const { table } = require('./helpers/airtable');
 const formattedReturn = require('./helpers/formattedReturn');
-module.exports = async (event) => {
+
+exports.handler = async (event) => {
   const fields = JSON.parse(event.body);
   try {
     const createdCourse = await table.create([{ fields }]);
