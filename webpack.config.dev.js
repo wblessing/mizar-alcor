e6cb6e0a33fdac7c55f7ff96ae414259c4a1b8be
Bill Blessing <wblessing@macbook.attlocal.net>
2021-11-08 23:07:02 -0600

    webpack dev config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -24,7 +24,49 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.API_BASE_URL': JSON.stringify(process.env.API_BASE_URL),
+      'process.env.REACT_APP_AUTH0_DOMAIN': JSON.stringify(
+        process.env.REACT_APP_AUTH0_DOMAIN
+      ),
+      'process.env.REACT_APP_AUTH0_CLIENT_ID': JSON.stringify(
+        process.env.REACT_APP_AUTH0_CLIENT_ID
+      ),
+      'process.env.REACT_APP_AUTH0_CALLBACK': JSON.stringify(
+        process.env.REACT_APP_AUTH0_CALLBACK
+      ),
+      'process.env.REACT_APP_AUTH0_AUDIENCE': JSON.stringify(
+        process.env.REACT_APP_AUTH0_AUDIENCE
+      ),
+      'process.env.REACT_APP_JWT_NAMESPACE': JSON.stringify(
+        process.env.REACT_APP_JWT_NAMESPACE
+      ),
+      'process.env.REACT_APP_SERVERLESS_URL': JSON.stringify(
+        process.env.REACT_APP_SERVERLESS_URL
+      ),
+      'process.env.FUNC_AIRTABLE_KEY': JSON.stringify(
+        process.env.FUNC_AIRTABLE_KEY
+      ),
+      'process.env.FUNC_AIRTABLE_AUTHORS_TABLE': JSON.stringify(
+        process.env.FUNC_AIRTABLE_AUTHORS_TABLE
+      ),
+      'process.env.FUNC_AIRTABLE_BASE_ID': JSON.stringify(
+        process.env.FUNC_AIRTABLE_BASE_ID
+      ),
+      'process.env.FUNC_AIRTABLE_COURSES_TABLE': JSON.stringify(
+        process.env.FUNC_AIRTABLE_COURSES_TABLE
+      ),
+      'process.env.FUNC_AUTH0_AUDIENCE': JSON.stringify(
+        process.env.FUNC_AUTH0_AUDIENCE
+      ),
+      'process.env.FUNC_AUTH0_ISSUER': JSON.stringify(
+        process.env.FUNC_AUTH0_ISSUER
+      ),
+      'process.env.FUNC_AUTH0_JWKS_URI': JSON.stringify(
+        process.env.FUNC_AUTH0_JWKS_URI
+      ),
+      'process.env.FUNC_AUTH0_JWT_NAMESPACE': JSON.stringify(
+        process.env.FUNC_AUTH0_JWT_NAMESPACE
+      ),
+      'process.env.FUNC_MONGO_URL': JSON.stringify(process.env.FUNC_MONGO_URL),
     }),
     new HtmlWebpackPlugin({
       template: 'public/index.html',
