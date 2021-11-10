fdb4c98a8e8acddb91df1bd464981b9dd16a29f4
wblessing <jwblessing@att.net>
2021-11-10 16:41:41 -0600

    course list styles

--- src/pages/courses/CourseList.js
+++ src/pages/courses/CourseList.js
@@ -5,47 +5,49 @@ import DangerButton from '../../components/common/DangerButton';
 import StandardButton from '../../components/common/StandardButton';
 
 const CourseList = ({ courses, onDeleteClick, onWatchClick }) => (
-  <table className="table">
-    <thead>
-      <tr>
-        <th />
-        <th>Title</th>
-        <th>Author</th>
-        <th>Category</th>
-        <th />
-      </tr>
-    </thead>
-    <tbody>
-      {courses.map((course) => {
-        return (
-          <tr key={course.title}>
-            <td>
-              <StandardButton
-                text="Watch"
-                onClick={() => onWatchClick(course)}
-              />
-            </td>
-            <td>
-              <Link to={'/course/' + course.title}>{course.title}</Link>
-            </td>
-            <td>{course.authorName}</td>
-            <td>{course.category}</td>
-            <td>
-              <a className="btn btn-light" href={course.url}>
-                Watch
-              </a>
-            </td>
-            <td>
-              <DangerButton
-                text="Delete"
-                onClick={() => onDeleteClick(course)}
-              />
-            </td>
-          </tr>
-        );
-      })}
-    </tbody>
-  </table>
+  <section className="bg-white p-4 shadow-md rounded-md">
+    <table className="table">
+      <thead>
+        <tr>
+          <th />
+          <th>Title</th>
+          <th>Author</th>
+          <th>Category</th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        {courses.map((course) => {
+          return (
+            <tr key={course.title}>
+              <td>
+                <StandardButton
+                  text="Watch"
+                  onClick={() => onWatchClick(course)}
+                />
+              </td>
+              <td>
+                <Link to={'/course/' + course.title}>{course.title}</Link>
+              </td>
+              <td>{course.authorName}</td>
+              <td>{course.category}</td>
+              <td>
+                <a className="btn btn-light" href={course.url}>
+                  Watch
+                </a>
+              </td>
+              <td>
+                <DangerButton
+                  text="Delete"
+                  onClick={() => onDeleteClick(course)}
+                />
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  </section>
 );
 
 CourseList.propTypes = {
