a8300cb2b6d9bc91474224fde4a62f433fca7fa6
wblessing <jwblessing@att.net>
2021-11-10 20:17:18 -0600

    styling of course form

--- src/components/common/SelectInputBoot.js
+++ src/components/common/SelectInputBoot.js
@@ -19,7 +19,10 @@ const SelectInputBoot = ({
           name={name}
           value={value}
           onChange={onChange}
-          className="form-control"
+          className="appearance-none rounded-none relative block w-full px-3
+            py-2 border border-gray-300 placeholder-gray-500 text-gray-900
+            rounded-md focus:outline-none focus:shadow-outline-blue
+            focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
         >
           <option value="">{defaultOption}</option>
           {options.map((option) => {
