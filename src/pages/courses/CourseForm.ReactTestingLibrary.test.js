81773b7f8015640d3f33fa18d39ef3e5d52dbc6f
Bill Blessing <wblessing@macbook.attlocal.net>
2021-11-12 20:46:01 -0600

    update tests

--- src/pages/courses/CourseForm.ReactTestingLibrary.test.js
+++ src/pages/courses/CourseForm.ReactTestingLibrary.test.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import CourseForm from "./CourseForm";
+import React from 'react';
+import { render } from '@testing-library/react';
+import CourseForm from './CourseForm';
 
 function renderCourseForm(args) {
   let defaultProps = {
@@ -9,24 +9,24 @@ function renderCourseForm(args) {
     saving: false,
     errors: {},
     onSave: () => {},
-    onChange: () => {}
+    onChange: () => {},
   };
 
   const props = { ...defaultProps, ...args };
   return render(<CourseForm {...props} />);
 }
 
-it("should render Add Course header", () => {
+it('should render Add Course header', () => {
   const { getByText } = renderCourseForm();
-  getByText("Add Course");
+  getByText('Add Course');
 });
 
 it('should label save button as "Save" when not saving', () => {
   const { getByText } = renderCourseForm();
-  getByText("Save");
+  getByText('Save');
 });
 
 it('should label save button as "Saving..." when saving', () => {
   const { getByText } = renderCourseForm({ saving: true });
-  getByText("Saving...");
+  getByText('Saving...');
 });
