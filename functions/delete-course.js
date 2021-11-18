3572a6b430704f27989c9751abd50600278cb6ff
wblessing <jwblessing@att.net>
2021-11-18 17:19:54 -0600

    tweak course permissions

--- functions/delete-course.js
+++ functions/delete-course.js
@@ -1,25 +1,28 @@
 const { coursesTable } = require('./helpers/airtable');
-const { requireAuth } = require('../lib/auth');
+const { requireScope } = require('../lib/auth');
 
-exports.handler = requireAuth(async (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  const { id } = JSON.parse(event.body);
-  try {
-    const deletedCourse = await coursesTable.destroy([id]);
-    return callback(null, {
-      statusCode: 200,
-      body: JSON.stringify({
-        deletedItem: deletedCourse,
-        message: 'Course deleted!',
-      }),
-    });
-  } catch (err) {
-    console.log('the err', err);
-    return callback(null, {
-      statusCode: 400,
-      body: JSON.stringify({
-        error: err,
-      }),
-    });
+exports.handler = requireScope(
+  'delete:course',
+  async (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    const { id } = JSON.parse(event.body);
+    try {
+      const deletedCourse = await coursesTable.destroy([id]);
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          deletedItem: deletedCourse,
+          message: 'Course deleted!',
+        }),
+      });
+    } catch (err) {
+      console.log('the err', err);
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: err,
+        }),
+      });
+    }
   }
-});
+);
