905ad5425494c358fe5369ca0c7d54dbac941f2d
Bill Blessing <37204075+wblessing@users.noreply.github.com>
2021-11-19 17:26:00 -0600

    Update CourseForm.js

--- src/pages/courses/CourseForm.js
+++ src/pages/courses/CourseForm.js
@@ -15,56 +15,61 @@ const CourseForm = ({
   errors = {},
 }) => {
   return (
-    <section className={classnames.FORM_BORDER}>
-      <form onSubmit={onSave}>
-        <PageTitle title={course.id ? 'Edit Course' : 'Add Course'} />
-        {errors.onSave && (
-          <div className="alert alert-danger" role="alert">
-            {errors.onSave}
+    <>
+      <PageTitle title={course.id ? 'Edit Course' : 'Add Course'} />
+      <div className={classnames.FORM_BORDER}>
+        <form onSubmit={onSave}>
+          {errors.onSave && (
+            <div className="alert alert-danger" role="alert">
+              {errors.onSave}
+            </div>
+          )}
+          <TextInput
+            name="title"
+            label="Title"
+            value={course.title}
+            onChange={onChange}
+            error={errors.title}
+          />
+
+          <SelectInput
+            name="authorId"
+            label="Author"
+            value={course.authorId || ''}
+            defaultOption="Select Author"
+            options={authors.map((author) => ({
+              value: author.externalId,
+              text: author.name,
+            }))}
+            onChange={onChange}
+            error={errors.author}
+          />
+
+          <TextInput
+            name="url"
+            label="Url"
+            value={course.url}
+            onChange={onChange}
+            error={errors.url}
+          />
+
+          <TextInput
+            name="category"
+            label="Category"
+            value={course.category}
+            onChange={onChange}
+            error={errors.category}
+          />
+
+          <div className="w-full sm:w-1/4 mt-4">
+            <GradientButton
+              type="submit"
+              text={saving ? 'Saving...' : 'Save'}
+            />
           </div>
-        )}
-        <TextInput
-          name="title"
-          label="Title"
-          value={course.title}
-          onChange={onChange}
-          error={errors.title}
-        />
-
-        <SelectInput
-          name="authorId"
-          label="Author"
-          value={course.authorId || ''}
-          defaultOption="Select Author"
-          options={authors.map((author) => ({
-            value: author.externalId,
-            text: author.name,
-          }))}
-          onChange={onChange}
-          error={errors.author}
-        />
-
-        <TextInput
-          name="url"
-          label="Url"
-          value={course.url}
-          onChange={onChange}
-          error={errors.url}
-        />
-
-        <TextInput
-          name="category"
-          label="Category"
-          value={course.category}
-          onChange={onChange}
-          error={errors.category}
-        />
-
-        <div className="w-full sm:w-1/4 mt-4">
-          <GradientButton type="submit" text={saving ? 'Saving...' : 'Save'} />
-        </div>
-      </form>
-    </section>
+        </form>
+      </div>
+    </>
   );
 };
 
